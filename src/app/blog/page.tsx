98d9fbf7d86990ce612abdd63bf77a660f55cb42
Nabeel Haider Rizvi <52364138+NabeelHR@users.noreply.github.com>
2024-04-02 17:37:46 -0700

    uploading initial data

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import colorsJson from '../../data/tbd.json'; // This import style requires "esModuleInterop", see "side notes"
+
 const Blog = () => {
+	console.log(colorsJson);
 	return (
 		<div className='prose lg:w-2/3 mx-auto'>
 			<h3>Some personal random interests about me:</h3>
+			{colorsJson[0].sport_type}
 			<p>i like to run</p>
 		</div>
 	);
