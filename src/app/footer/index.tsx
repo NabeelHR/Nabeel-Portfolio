e373dc6f4d5484b4a5762df16915f0f803f41c15
Nabeel Haider Rizvi <52364138+NabeelHR@users.noreply.github.com>
2024-04-05 00:34:37 -0700

    footer content

--- src/app/footer/index.tsx
+++ src/app/footer/index.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import SocialLink from '../professional/socialLink';
-import { LinkedInIcon, GitHubIcon } from '../../../public/icons';
-import { GitHubUrl, LinkedInUrl } from '@/data/personalInfo';
+import { LinkedInIcon, GitHubIcon, MailIcon } from '../../../public/icons';
+import { GitHubUrl, LinkedInUrl, EmailAddr } from '@/data/personalInfo';
 
 const Footer = () => {
 	return (
 		<div className='flex bg-primary w-full mt-auto'>
 			<div className='h-24 p-4'>
-				<ul role='list'>
+				<ul>
 					<SocialLink href={LinkedInUrl} icon={LinkedInIcon} className='mt-4'>
 						Connect with Linkedin
 					</SocialLink>
-					<SocialLink href={GitHubUrl} icon={LinkedInIcon} className='mt-4'>
+					<SocialLink href={GitHubUrl} icon={GitHubIcon} className='mt-4'>
 						GitHub
+					</SocialLink>
+					<SocialLink
+						href={`mailto:${EmailAddr}`}
+						icon={MailIcon}
+						className='mt-4'>
+						Contact via email
 					</SocialLink>
 				</ul>
 			</div>
