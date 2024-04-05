81d558b3e26d21c8c18a1b57f0cc0537f480a3f8
Nabeel Haider Rizvi <52364138+NabeelHR@users.noreply.github.com>
2024-04-05 02:09:09 -0700

    format buttons

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -142,25 +142,27 @@ const Blog = () => {
 					))}
 				</tbody>
 			</table>
-			<button
-				className='border rounded p-1'
-				onClick={() => table.previousPage()}
-				disabled={!table.getCanPreviousPage()}>
-				{'<'}
-			</button>
-			<button
-				className='border rounded p-1'
-				onClick={() => table.nextPage()}
-				disabled={!table.getCanNextPage()}>
-				{'>'}
-			</button>
-			<span className='flex items-center gap-1'>
-				<div>Page</div>
-				<strong>
-					{table.getState().pagination.pageIndex + 1} of{' '}
-					{table.getPageCount().toLocaleString()}
-				</strong>
-			</span>
+			<div className='flex justify-evenly mx-auto'>
+				<button
+					className='border rounded p-1'
+					onClick={() => table.previousPage()}
+					disabled={!table.getCanPreviousPage()}>
+					{'<'}
+				</button>
+				<span className='flex items-center gap-1'>
+					<div>Page</div>
+					<strong>
+						{table.getState().pagination.pageIndex + 1} of{' '}
+						{table.getPageCount().toLocaleString()}
+					</strong>
+				</span>
+				<button
+					className='border rounded p-1'
+					onClick={() => table.nextPage()}
+					disabled={!table.getCanNextPage()}>
+					{'>'}
+				</button>
+			</div>
 		</div>
 	);
 };
